0042b0caf0c6472931cdf55e0646fc36cbb7772b
zFL1CKz <dimakukharev2002@yandex.ru>
2022-08-14 00:59:26 +0300

    some fix

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,15 +1,17 @@
 import React, {FC} from 'react'
 import {useGetAllCategoriesQuery} from '../services/categoryApi'
+import {API_URL} from '../config'
 
 const Profile: FC = () => {
   const {data: categories} = useGetAllCategoriesQuery('')
-  categories?.map(item => console.log(item.icon))
+  console.log(categories)
 
   return (
     <div>
       {categories?.map(category =>
         <div key={category._id}>
-          <img src={category.icon} height='24' width='24' alt=''/>
+          <img src={`${API_URL}/category-icon/${category.icon}`} height='24' width='24' alt=''/>
+
         </div>
       )}
     </div>
--- server/models/category-model.js
+++ server/models/category-model.js
@@ -2,7 +2,7 @@ const {Schema, model, Types} = require('mongoose')
 
 const CategorySchema = new Schema({
   name: {type: String, required: true},
-  icon: {type: String, default: `${process.env.STATIC_PATH}\\category-icon\\default.svg`},
+  icon: {type: String, default: 'default.svg'},
   color: {type: String, default: '#8687E7'},
   user: {type: Types.ObjectId, ref: 'User', default: null}
 })
